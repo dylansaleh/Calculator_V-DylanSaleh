9ad446fec5d4f07ba9917c9f527240f9d9871de6
Unknown <dylansaleh98@gmail.com>
2017-11-15 23:25:15 +0100

    Added pictures, comments and account

--- Testcalculator.c
+++ Testcalculator.c
@@ -1,26 +1,31 @@
 #include "unity\src\unity.h"
 #include "calculator.h"
 
+//Tests the function, Addition()
 void test_Additon(void)
 {
 TEST_ASSERT_EQUAL_FLOAT(4.2, Addition(2.2, 2));
 }
 
+//Tests the function, Subtraction()
 void test_Subtraction(void)
 {
 TEST_ASSERT_EQUAL_FLOAT(2.2, Subtraction(4.2, 2));
 }
 
+//Tests the function, Multiplication()
 void test_Multiplication(void)
 {
 TEST_ASSERT_EQUAL_FLOAT(8.4, Multiplication(4.2, 2));
 }
 
+//Tests the function, Division()
 void test_Division(void)
 {
 TEST_ASSERT_EQUAL_FLOAT(4.2, Division(8.4, 2));
 }
 
+//Starts Unity, Runs tests, Ends Unity
 int main(void)
 {
 UNITY_BEGIN();
--- calculator.c
+++ calculator.c
@@ -1,18 +1,22 @@
 #include "calculator.h"
 #include <stdio.h>
 
+//Sums float variables
 float Addition(float a, float b) {
    return a + b;
 }
 
+//Subtracts float variables
 float Subtraction(float a, float b) {
    return a - b;
 }
 
+//Divides float variables
 float Division(float a, float b) {
    return a / b;
 }
 
+//Multiplies float variables
 float Multiplication(float a, float b) {
    return a * b;
 }
